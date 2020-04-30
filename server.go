914ac80e0636f51159c285ecbb61f44cbadb93a6
Neeraj Prasad <nprasad@mit.edu>
2020-04-30 01:41:22 -0400

    round-trip comm

--- server.go
+++ server.go
@@ -22,14 +22,13 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	http.ServeFile(w, r, "home.html")
 }
 
 func main() {
 	flag.Parse()
 	hub := newHub()
 	go hub.run()
-	http.HandleFunc("/", serveHome)
+	http.Handle("/", http.FileServer(http.Dir("web_app/")))
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
--- web_app/login.js
+++ web_app/login.js
@@ -1,52 +1,36 @@
-var conn;
-var msg = document.getElementById("msg");
-var log = document.getElementById("log");
+let conn;
+let body = document.querySelector("body");
 
-
-
-function appendLog(item) {
-    var doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
-    log.appendChild(item);
-    if (doScroll) {
-        log.scrollTop = log.scrollHeight - log.clientHeight;
+document.querySelector("form").onsubmit = function () {
+    let room = document.querySelector('input[name="room"]');
+    let password = document.querySelector('input[name="password"]');
+    if (!conn) {
+        return false;
     }
+    if (!room || !password) {
+        return false;
+    }
+    let msg = JSON.stringify({"room": room.value, "password": password.value});
+    conn.send(msg);
+    room.value = "";
+    password.value = "";
+    return false;
+};
+
+if (window["WebSocket"]) {
+    conn = new WebSocket("ws://" + document.location.host + "/ws");
+    conn.onclose = function (evt) {
+
+    };
+    conn.onmessage = function (evt) {
+        let div = document.createElement("div");
+        div.innerHTML = evt.data;
+        body.append(div);
+    };
+} else {
+
+    let item = document.createElement("div");
+    item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
+    body.append(item);
 }
 
-window.onload = function () {
-
-
-
-
-    document.getElementById("form").onsubmit = function () {
-        if (!conn) {
-            return false;
-        }
-        if (!msg.value) {
-            return false;
-        }
-        conn.send(msg.value);
-        msg.value = "";
-        return false;
-    };
-
-    if (window["WebSocket"]) {
-        conn = new WebSocket("ws://" + document.location.host + "/ws");
-        conn.onclose = function (evt) {
-            var item = document.createElement("div");
-            item.innerHTML = "<b>Connection closed.</b>";
-            appendLog(item);
-        };
-        conn.onmessage = function (evt) {
-            var messages = evt.data.split('\n');
-            for (var i = 0; i < messages.length; i++) {
-                var item = document.createElement("div");
-                item.innerText = messages[i];
-                appendLog(item);
-            }
-        };
-    } else {
-        var item = document.createElement("div");
-        item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
-        appendLog(item);
-    }
-};
